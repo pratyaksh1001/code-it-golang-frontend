87ee5ee86dc68b517f15bcd3c3472aeca36c8521
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2026-07-07 15:38:58 +0530

    deployment configs

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:9000",
     headers: {
         "Content-Type": "application/json",
     },
